2ad01e817076ac15a1888f911c82f7fffeb4e7eb
Carmen Su <csu30@my.bcit.ca>
2022-11-15 09:45:06 -0800

    function practice

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+// function greet(userName) {
+//     console.log(`Hello ${userName}`)
+// }
+
+// greet("Neda");
+
+// let greet = function(userName) {
+
+//     console.log(`Hello ${userName}`)
+// };
+
+let greet = (userName) => {
+    console.log(`Hello ${userName}`)
+}
+
+greet("Neda");
