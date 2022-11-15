19084eb14ae245db6a7dbe42d6752f1275c616db
Carmen Su <csu30@my.bcit.ca>
2022-11-15 10:44:21 -0800

    populate shopping list

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
-// function greet(userName) {
-//     console.log(`Hello ${userName}`)
+// function greet(userName, time = "day") {
+//     return(`Good ${time} ${userName}`)
 // }
 
+// const result = greet("Carmen", "Night");
+// console.log(result)
 // greet("Neda");
 
 // let greet = function(userName) {
@@ -9,8 +11,33 @@
 //     console.log(`Hello ${userName}`)
 // };
 
-let greet = (userName) => {
-    console.log(`Hello ${userName}`)
-}
+// let greet = (userName) => {
+//     console.log(`Hello ${userName}`)
+// }
 
-greet("Neda");
+// greet("Neda");
+
+// makeNoise();
+
+// function makeNoise() {
+//     console.log("Pling!")
+// }
+
+const unorderedListElement = document.querySelector(".shopping");//DOM element
+function populateList (list)
+{
+    // for (let item of list)
+    // {
+
+    // }
+
+    for (let i = 0; i < list.length; i ++)
+    {
+        console.log(list[i])
+        const listItemElement = document.createElement("li");
+        listItemElement.textContent = list[i];
+        unorderedListElement.appendChild(listItemElement);
+    }
+}
+let shoppingList = ["cheese", "bread", "green pepper"];
+populateList(shoppingList);
